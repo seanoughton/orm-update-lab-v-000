a7b7dd2b04ce435c5341978e59080ac7721f8c18
Sean Oughton <seanoughton@gmail.com>
2018-03-13 17:11:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,57 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name,grade,id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def update
+    sql = "UPDATE students set name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql,self.name,self.grade,self.id)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name,grade) VALUES (?,?)
+      SQL
+      DB[:conn].execute(sql,self.name,self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() from students")[0][0]
+    end
+  end
+
+  def self.create(name,grade)
+    student = self.new(name,grade)
+    student.save
+  end
+
+  def new_from_db
+  end
 
 end

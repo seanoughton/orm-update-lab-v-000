9e1fa8dfccafa8cf6dadcd7b478c639e6465f238
Sean Oughton <seanoughton@gmail.com>
2018-03-13 17:31:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,7 @@ class Student
       SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
     DB[:conn].execute(sql,name).collect do |row|
-      self.new_from_db
+      self.new_from_db(row)
     end
 
   end
